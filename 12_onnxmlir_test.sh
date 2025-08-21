aebebab14d7345571ec1e001e05151f298e04439
Uuuuuuho <44142790+Uuuuuuho@users.noreply.github.com>
2025-08-21 09:49:14 +0000

    Added 12_onnxmlir_test.sh

---
+++ 12_onnxmlir_test.sh
@@ -0,0 +1,4 @@
+# !/usr/bin/bash
+
+python 11_onnx_to_mlir.py --in onnx_out/SymmetricFT.onnx --out onnx_out/SymmetricFT.mlir --opt-level O0
+python 09_mlir_to_onnx.py --in onnx_out/SymmetricFT.mlir --out onnx_out/SymmetricFT_opt.onnx
